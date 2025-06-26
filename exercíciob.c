6415b7305fe9bd78f1c24cca50b07ada2c78ec08
sabjoao <sabino.jp03@gmail.com>
2025-06-26 10:15:13 -0300

    Código exercício B

--- exercíciob.c
+++ exercíciob.c
@@ -1,3 +1,16 @@
-//
-// Created by Casa on 26/06/2025.
-//
+#include <stdio.h>
+
+int main()
+{
+    int ano;
+    int anoerrado;
+    int idade;
+    scanf("%d", &ano);
+    if((2010<=ano)&&(ano<=2020))
+    {
+        idade = 2025 - ano;
+        anoerrado = 2025 - (idade*2);
+        printf("%d", anoerrado);
+    }
+    return 0;
+}
